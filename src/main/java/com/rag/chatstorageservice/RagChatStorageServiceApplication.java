b2630ba38fc6c212b4402f2dc86587ce12dbe850
harishvudayagiri <harishvudayagiri@gmail.com>
2026-01-02 22:37:42 +0530

    rag-chat-storage-service

---
+++ src/main/java/com/rag/chatstorageservice/RagChatStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rag.chatstorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagChatStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagChatStorageServiceApplication.class, args);
+    }
+
+}
